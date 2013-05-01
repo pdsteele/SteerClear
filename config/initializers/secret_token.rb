09e0e79acdd7288bf71d80d98a5978c54ac62e94
Philip Steele <pdsteele@wm.edu>
2013-05-01 19:11:53 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SteerClear::Application.config.secret_token = 'd1c1c490a749f18a04ac7e7782c9ca40850065eef18568c8280b0bc8f9ddc0f22001930e791ed51776e3c95f076982d18c7236c8d3c28d03f1ce66a4e543fba6'
