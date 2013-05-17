716f409f16663b10cccd36edf854e34c4b7a770f
Philip Steele <pdsteele@wm.edu>
2013-05-17 17:32:26 -0400

    fixes for heroku mailing

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -68,4 +68,27 @@ SteerClear::Application.configure do
   #FOR ACTIVE ADMIN
   config.action_mailer.default_url_options = { :host => '#' } #Placeholder in host until determined
   config.assets.initialize_on_precompile = false
+
+  # for mailing on heroku
+  # Disable delivery errors, bad email addresses will be ignored
+  ActionMailer::Base.smtp_settings = {
+      :address        => 'smtp.sendgrid.net',
+      :port           => '587',
+      :authentication => :plain,
+      :user_name      => ENV['SENDGRID_USERNAME'],
+      :password       => ENV['SENDGRID_PASSWORD'],
+      :domain         => 'heroku.com',
+      :enable_starttls_auto => true
+  }
+  #config.action_mailer.raise_delivery_errors = true
+  #config.action_mailer.delivery_method = :smtp
+  #config.action_mailer.default_url_options = { :host => 'steerclear.heroku.com' }
+  #ActionMailer::Base.smtp_settings = {
+  #    :address    => "smtp.sendgrid.net",
+  #    :port       => 25,
+  #    :user_name  => ENV['SENDGRID_USERNAME'],
+  #    :password   => ENV['SENDGRID_PASSWORD'],
+  #    :domain     => ENV['SENDGRID_DOMAIN'],
+  #    :authentication  => :plain
+  #}
 end
